428b8c7b25e5890baab5f40b13f70ef7d1ada1c0
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-01-06 17:53:26 +0100

    Creating cube

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ class App {
 
 	resize() {
 		this.camera.aspect = window.innerWidth / window.innerHeight;
-		this.camera.updateProjectionMatrix;
+		this.camera.updateProjectionMatrix();
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
 	}
 
