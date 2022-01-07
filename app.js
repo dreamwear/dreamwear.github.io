e2b91f203600adb43acf3f113f0d6518eea5c178
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-01-07 12:36:24 +0100

    Geometry

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ class App {
         // const geometry = new THREE.TorusKnotGeometry(3, 1, 100, 16);
         const shape = new THREE.Shape();
         const outerRadius = 0.8;
-        const innerRadius = 0.4;
+        const innerRadius = 0.2;
         const PI2 = Math.PI * 2;
         const inc = PI2 / 10;
 
